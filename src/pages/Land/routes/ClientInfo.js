3da962ffe3a038c7cc34a103c36355bf473c49bc
ader1990 <webmaster@lazyprogrammer.io>
2022-01-09 03:23:56 +0100

    land: add more fields

--- src/pages/Land/routes/ClientInfo.js
+++ src/pages/Land/routes/ClientInfo.js
@@ -30,19 +30,23 @@ export default function ClientRealm() {
         <>
             <Box className={classes.sortWrapper}>
                 <Box className={classes.sortInner}>
-                    <Typography variant='subtitle1' sx={{ marginRight: '12px' }}>Aggregated Gotchies XP: { addressInfo.gotchies_xp } </Typography>
+                    <Typography variant='subtitle1' sx={{ marginRight: '12px' }}>Gotchies estimated value: { parseInt(addressInfo.all_gotchies_estimated_price)} GHST</Typography>
                 </Box>
 
                 <Box className={classes.sortInner}>
-                    <Typography variant='subtitle1' className={classes.sortText}>Average Gotchies XP: {addressInfo.gotchies_medium_xp } </Typography>
+                    <Typography variant='subtitle1' sx={{ marginRight: '12px' }}>Items estimated value: { parseInt(addressInfo.all_items_estimated_price)} GHST</Typography>
                 </Box>
 
                 <Box className={classes.sortInner}>
-                    <Typography variant='subtitle1' className={classes.sortText}>Aggregated Gotchies Kinship: {addressInfo.gotchies_kinship } </Typography>
+                    <Typography variant='subtitle1' sx={{ marginRight: '12px' }}>All gotchies XP: { addressInfo.gotchies_xp } ({addressInfo.gotchies_medium_xp}/gotchi)</Typography>
                 </Box>
 
                 <Box className={classes.sortInner}>
-                    <Typography variant='subtitle1' className={classes.sortText}>Average Gotchies Kinship: {addressInfo.gotchies_medium_kinship } </Typography>
+                    <Typography variant='subtitle1' className={classes.sortText}>All gotchies Kinship: {addressInfo.gotchies_kinship } ({addressInfo.gotchies_medium_kinship}/gotchi)</Typography>
+                </Box>
+
+                <Box className={classes.sortInner}>
+                    <Typography variant='subtitle1' className={classes.sortText}>Gotchies estimated reward: {parseInt(4 * addressInfo.estimated_full_payment_first_round) } GHST</Typography>
                 </Box>
             </Box>
 
